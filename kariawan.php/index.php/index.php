3ec658ce87275048a9c8d3be05e103d82fdc91dc
amild0123 <Amild384@gmail.com>
2025-01-15 14:21:50 +0700

    P

---
+++ kariawan.php/index.php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<HTML lang="en">
+    <head>
+        <title> documen </title>
+    </head>
+
+    <body>
+        <h3>
+            <?php
+            echo "gaji pokok kariawan sebesar Rp5.000.000. </br>";
+
+             ?>
+         </h3>
+    </body>
+</html>
