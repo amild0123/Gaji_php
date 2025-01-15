00a1d89d468bd699321e845c0a9824e1f5a2df1e
amild0123 <Amild384@gmail.com>
2025-01-15 16:15:50 +0700

    p

--- kariawan.php/index.php/index.php
+++ kariawan.php/index.php/index.php
@@ -8,7 +8,26 @@
         <h3>
             <?php
             echo "gaji pokok kariawan sebesar Rp5.000.000. </br>";
+            echo "setiap projek mendapat bonus sebesar Rp.500.000 </br>";
+            
+            $proyek = 5;
+            define("gaji",5000000);
+            define("pajak",0.12);
+            define("bonus",500000);
 
+
+            echo "jumlah proyek yang telah di selesai adalah 5 proyek ";
+            echo "jumlah bonus ".number_format($bonus = $proyek * bonus,0,',','.');
+            echo "</br>";
+            echo "</br>";
+            echo "rincian gaji bersih kariawan pertama </br>";
+            echo "gaji pokok Rp5.000.000 ditambah bonus".number_format($gajiKotor = gaji + $bonus ,0,',','.');
+            echo "</br>";
+          //  $gaji += $proyek;
+            echo "gaji bersih ".number_format($gajiBersih= $gajiKotor - ($gajiKotor * pajak),0,',','.');
+            echo "</br>";
+            
+          
              ?>
          </h3>
     </body>
